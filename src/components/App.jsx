285156db8a05bb5691d6c33ae1f4adb65092f597
alexgoit <alexandrpuryaev@gmail.com>
2023-07-25 15:46:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,23 +7,37 @@ import Statistics from './Statistics';
 document.title = 'HW-4 Feedback';
 
 const App = () => {
-  const [state, setState] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const options = { good, neutral, bad };
 
   const onClick = option => {
-    setState({ ...state, [option]: state[option] + 1 });
+    switch (option) {
+      case 'good':
+        setGood(good + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+
+      case 'bad':
+        setBad(bad + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
   return (
     <Section title="Please leave feedback">
       <FeedbackOptions
         onClick={onClick}
-        feedbackOptions={state}
+        feedbackOptions={options}
       ></FeedbackOptions>
-      <Statistics feedbackOptions={state} />
+      <Statistics feedbackOptions={options} />
     </Section>
   );
 };
